f5a3d97b9f875a6a809ad7a64c1595522fe7a2e5
Lance-Fuentes <lfuentes.tech@gmail.com>
2024-03-05 17:08:22 -0600

    Implemented model associations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ApplicationRecord
+  has_many :recipe_authors
+  has_many :recipes, through: :recipe_authors
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ApplicationRecord
   belongs_to :category
+
+  has_many :recipe_tags
+  has_many :tags, through: :recipe_tags
+
+  has_many :recipe_authors
+  has_many :authors, through: :recipe_authors
+
+  has_one :recipe_detail
 end
